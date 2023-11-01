16f1216311257c3da7fb61c94afbdc9d109e4d50
Arina Astanina <timyr.kiyan@gmail.com>
2023-11-01 10:13:02 +0300

    fixes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from develop import utils
 
+
 def main():
     filename = 'operations.json'
     amount_of_operations = int(input('Введите количество последних операций (не более 5): '))
 
     if amount_of_operations < 6:
-        data = utils.get_dat(filename)
+        data = utils.get_data(filename)
         operations_executed = utils.get_operations_executed(data)
         last_num_operations = utils.get_last_five_operations(operations_executed, amount_of_operations)
         operations_formatted = utils.get_operations_formatted(last_num_operations)
@@ -16,6 +17,7 @@ def main():
     else:
         print("Запросите 5 или менее последних операций")
         return main()
+
 
 if __name__ == "__main__":
     main()
